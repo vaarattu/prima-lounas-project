c2083a9622a6b00c05a7d8a44db7b388b88c72a1
Juha Ala-Rantala <alarantala.juha@gmail.com>
2021-09-18 11:20:45 +0300

    works

--- prima-lounas-flutter/lib/utils/constants.dart
+++ prima-lounas-flutter/lib/utils/constants.dart
@@ -1,2 +1,2 @@
 const localhostURL = "http://localhost:8888/api/v1/week";
-const raspiUrl = "http://192.168.38.115:8888/api/v1/menu";
+const raspiUrl = "http://primalounas.duckdns.org:8888/api/v1/menu";
